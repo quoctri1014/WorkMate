1399f8d3b010f8a5aa36e8f488b39dc84db4530f
quoctri1014 <quoctri101405@gmail.com>
2026-05-25 03:49:04 +0700

    fix(admin): resolve invalid unicode escape sequence in imports caused by translation script

--- workmate_admin/src/views/LoginView.jsx
+++ workmate_admin/src/views/LoginView.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useTranslation } from \'react-i18next\';
+import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 import { motion } from 'framer-motion';
 import { Icon, API_URL } from '../components/Common';
